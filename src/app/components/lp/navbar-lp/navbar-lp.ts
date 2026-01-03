e3f952c3a31ca0b1fbd479d902c1e6476a1ac98a
Muhammad Irpan Hafiana <irpanhafiana@gmail.com>
2026-01-04 01:58:11 +0700

    .

---
+++ src/app/components/lp/navbar-lp/navbar-lp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-lp',
+  imports: [],
+  templateUrl: './navbar-lp.html',
+  styleUrl: './navbar-lp.css',
+})
+export class NavbarLp {}
